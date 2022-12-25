3320da11f974d01e002e68f433b6bef8921f09ee
Sujudi Hanif <sujudihanif36@gmail.com>
2022-12-25 11:16:47 +0700

    Add project files.

---
+++ Tetris/TBlock.cs
@@ -0,0 +1,15 @@
+﻿namespace Tetris
+{
+    internal class TBlock : Block
+    {
+        public override int Id => 6;
+        protected override Position StartOffset => new(0, 3);
+        protected override Position[][] Tiles { get; } = new Position[][]
+        {
+            new Position[] { new(0,1), new(1,0), new(1,1), new(1,2) },
+            new Position[] { new(0,1), new(1,1), new(1,2), new(2,1) },
+            new Position[] { new(1,0), new(1,1), new(1,2), new(2,1) },
+            new Position[] { new(0,1), new(1,0), new(1,1), new(2,1) }
+        };
+    }
+}
